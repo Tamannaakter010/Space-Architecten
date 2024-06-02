001e60c88d2bcca117a803edfec71197b1b180a8
MAHTAB <tamannapinks010@gmail.com>
2024-06-02 12:11:41 +0600

    footer

--- app.js
+++ app.js
@@ -74,3 +74,24 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+/*---------------------------active------------------------*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href').includes(id)) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+}
